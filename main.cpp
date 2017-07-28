865d1f4019fed242fb258b038918c58d147e3f84
Zhiyao Yang <snowyang1111@gmail.com>
2017-07-28 10:55:45 -0400

    read and interpret input file, replace hpdm file
    next to run hpdm and allocate results

--- main.cpp
+++ main.cpp
@@ -4,69 +4,131 @@
 #include <QStringList>
 #include <iostream>
 #include <fstream>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
     //read input file and generate array of parameter, record output parameter
-
-
+    QString input1, input2, input3;
+    QStringList outputList,inputInfo1, inputInfo2, inputInfo3;
+    int run1 = 0, run2 = 0, run3 = 0;
+    double start1 = 0, start2 = 0, start3 = 0;
+    double interval1 = 0, interval2 = 0, interval3 = 0;
     QFile iFile("input.txt");
-    if(iFile.open(QIODevice::ReadOnly)){
-
-        if(!oFile.open(QIODevice::WriteOnly)){
-            std::cout<<"failed to open output file.\n";
-            write = false;
-        }
-        std::cout<<"input parameters are:\n";
+    if(iFile.open(QIODevice::ReadOnly|QIODevice::Text)){
 
         QTextStream in(&iFile);
 
-        std::string str;
-
-        QString ref_s, sorb_s, tK_s, xMass_s;
 
         while (!in.atEnd()){
             QString line = in.readLine();
-            if(line.startsWith("ref/sorb")){
+            if(line.startsWith("INPUT")){
 
-                if(write){
-                    out<<line<<'\n';
-                    out<<"t[K]\tmass ratio\tp[kPa]\tequation form\treference\n";
-                }
 
-                ref_s = line.split("\t").at(1);
-                sorb_s = line.split("\t").at(2);
-
-                ref = ref_s.toStdString();
-                sorb = sorb_s.toStdString();
+                input1 = line.split("\t").at(1);
+                input2 = line.split("\t").at(2);
+                input3 = line.split("\t").at(3);
             }
-            else if(line.at(0).isDigit()){
-                tK_s= line.split("\t").at(0);
-                tK = tK_s.toDouble();
-                xMass_s = line.split("\t").at(1);
-                xMass = xMass_s.toDouble();
+            else if(line.startsWith("OUTPUT")){
 
-
-                if(write){
-                    out<<QString::fromStdString(str);
-                }
+                outputList = line.split("\t");
+                outputList.removeAt(0);
             }
 
             in.flush();
             iFile.close();
         }
 
-
-        //replace parameter in .hpdm file and run calculation
-
-        //extract interested results from .out file and put into output file
+        qDebug()<<"inputs"<<input1<<input2<<input3;
+        qDebug()<<"outputs"<<outputList;
 
     }
+    else{
+        return 0;
+    }
+    inputInfo1 = input1.split(":");
+    inputInfo2 = input2.split(":");
+    inputInfo3 = input3.split(":");
+
+    qDebug()<<inputInfo1<<inputInfo2<<inputInfo3;
+
+    start1 = inputInfo1.at(2).toDouble();
+    interval1 = inputInfo1.at(3).toDouble();
+    run1 = inputInfo1.at(4).toInt();
+    start2 = inputInfo2.at(2).toDouble();
+    interval2 = inputInfo2.at(3).toDouble();
+    run2 = inputInfo2.at(4).toInt();
+    start3 = inputInfo3.at(2).toDouble();
+    interval3 = inputInfo3.at(3).toDouble();
+    run3 = inputInfo3.at(4).toInt();
+
+    //find corresponding rows in .hpdm file
+    //currently only ID w/ variable name, may fail w/ multiple component system
+    QString allText, searchLine;
+    QStringList lineList,line1,line2,line3;
+    QFile hFile("hpdata.hpdm");
+    if(hFile.open(QIODevice::ReadOnly|QIODevice::Text)){
+        QTextStream instream(&hFile);
+
+        allText = instream.readAll();
+        hFile.close();
+    }
+    lineList = allText.split("\n");
+    int inputLine1 = 0, inputLine2 = 0, inputLine3 = 0;
+    for(int i = 0; i < lineList.count();i++){
+        searchLine = lineList.at(i);
+        if(searchLine.contains(inputInfo1.at(1))){
+            inputLine1 = i;
+            line1 = searchLine.split("\t");
+        }
+        else if(searchLine.contains(inputInfo2.at(1))){
+            inputLine2 = i;
+            line2 = searchLine.split("\t");
+        }
+        else if(searchLine.contains(inputInfo3.at(1))){
+            inputLine3 = i;
+            line3 = searchLine.split("\t");
+        }
+    }
+    if(inputLine1 * inputLine2 * inputLine3 == 0){
+        qDebug()<<"failed to find input line"
+               <<inputLine1<<inputLine2<<inputLine3;
+    }
+    else{
+
+        qDebug()<<"found input line"
+               <<inputLine1<<inputLine2<<inputLine3;
+    }
+
+    //replace parameter in .hpdm file and run calculation
+    for(int o = 0; o < run1; o++){
+
+        line1.replace(6,QString::number(start1 + o * interval1));
+
+        for(int p = 0; p < run2; p++){
+
+            line2.replace(6,QString::number(start2 + p * interval2));
+
+            for(int q = 0; q < run3; q++){
+
+                line3.replace(6,QString::number(start3 + q * interval3));
+
+                qDebug()<<1<<line1.at(6)<<2<<line2.at(6)<<3<<line3.at(6);
+
+                //run hpdm
+                //extract interested results from .out file and put into output file
+            }
+        }
+    }
+
+
+
+    std::string exit;
 
     std::cout<<"\n\n exit?";
     std::cin>>exit;
 
-    return a.exec();
+    return 0;
 }
--- readme.md
+++ readme.md
@@ -2,17 +2,23 @@
 
 # I/O format:
 * input.txt, define one or two input parameters + start/interval; define output parameters
-	* NPARA	2
-	* INPUT	1:Vol_flow_rate_in:10:0.5:10	2:TI_C:75:1:5
+	* NPARA	3
+	* INPUT	1:Vol_flow_rate_in:10:0.5:10	2:TI_C:75:1:5	2:MI_C:0.5:0.2:3
 	* OUTPUT	6:TO_AIR	11:EER
 * output.txt, summarize results
-	* RUN	1:Vol_flow_rate_in	2:TI_C	6:TO_AIR	11:EER
-	* 1	10	75	0.5	5.6
-	* 2	10.5	75	0.55	5.8
+	* RUN	1:Vol_flow_rate_in	2:TI_C	2:MI_C	6:TO_AIR	11:EER
+	* 1	10	75	0.5	0.5	5.6
+	* 2	10.5	75	0.5	0.55	5.8
 	* ...
-	* 11	10	76	0.7	6.1
+	* 10	14.5	75	0.5	0.6	6.1
+	* 11	10	76	0.5	0.7	6.1
 	* ...
-	* 50	15	80	0.7	7.2
+	* 50	14.5	79	0.5	0.7	7.2
+	* 51	10	75	0.7	0.6	6.5
+	* ...
+	* 100	14.5	79	0.7	0.8	7.8
+	* ...
+	* 150	14.5	79	0.9 0.9	8.5
 		
 		
-### last updated 7/27/17
+### last updated 7/28/17
